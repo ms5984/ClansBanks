8f0d02aaa5130ebb3fb705cbb86b7d7a1b26e301
Matthew Sanders <msanders5984@gmail.com>
2021-03-17 02:57:55 -0400

    Test permission setup

--- ClansBanks-plugin/src/test/java/com/github/ms5984/clans/clansbanks/util/BanksPermissionTest.java
+++ ClansBanks-plugin/src/test/java/com/github/ms5984/clans/clansbanks/util/BanksPermissionTest.java
@@ -1,13 +1,21 @@
 package com.github.ms5984.clans.clansbanks.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BanksPermissionTest {
@@ -28,5 +36,28 @@ public class BanksPermissionTest {
         // test method
         assertFalse(commandSender.hasPermission(PERMISSION.node));
         assertTrue(PERMISSION.not(commandSender));
+    }
+
+    @Test
+    public void testSetup(@Mock PluginManager pluginManager, @Mock Server server) {
+        // preconfigure mock
+        doAnswer(invocation -> Collections.emptySet()).when(pluginManager).getPermissionSubscriptions(any());
+        // stub into server
+        doReturn(pluginManager).when(server).getPluginManager();
+        // setup fake server
+        doReturn("fakeServer").when(server).getName();
+        doReturn("fakeVersion").when(server).getVersion();
+        doReturn("fakeBukkitVersion").when(server).getBukkitVersion();
+        doReturn(Logger.getLogger("Test")).when(server).getLogger();
+        Bukkit.setServer(server);
+
+        // test success, should set init to true internally
+        BanksPermission.setup(pluginManager);
+        // verify attempts were made to add each permission to the PluginManager mock
+        Arrays.stream(BanksPermission.values())
+                .forEach(permission -> verify(pluginManager).addPermission(argThat(p -> p.getName().equals(permission.node))));
+
+        // assert setup throws IllegalStateException once permissions have already been added
+        assertThrows(IllegalStateException.class, () -> BanksPermission.setup(pluginManager));
     }
 }
