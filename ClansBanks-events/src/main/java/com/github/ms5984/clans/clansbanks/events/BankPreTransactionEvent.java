9583e015a16ce890a0436d65bb783d7b42b3b3e2
Matthew Sanders <msanders5984@gmail.com>
2021-04-30 15:32:13 -0400

    Change events module to Apache 2.0 (as we are NOT distributing the Event class)

--- ClansBanks-events/src/main/java/com/github/ms5984/clans/clansbanks/events/BankPreTransactionEvent.java
+++ ClansBanks-events/src/main/java/com/github/ms5984/clans/clansbanks/events/BankPreTransactionEvent.java
@@ -1,21 +1,21 @@
 /*
- *  Copyright 2020 ms5984 (Matt) <https://github.com/ms5984>
- *  Copyright 2020 Hempfest <https://github.com/Hempfest>
- *
  *  This file is part of ClansBanks-events.
  *
- *  ClansBanks-events is free software: you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
+ *  Copyright 2021 ms5984 (Matt) <https://github.com/ms5984>
+ *  Copyright 2020 Hempfest <https://github.com/Hempfest>
  *
- *  ClansBanks-events is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License or
+ *  the license mentioned above.
+ *  You may obtain a copy of the License at
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.github.ms5984.clans.clansbanks.events;
 
